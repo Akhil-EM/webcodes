46a765afb57090cb3b9d59db4b4f52c7bd3a9177
ManasSarovar32 <manassarovar32@gmail.com>
2019-11-14 16:40:55 +0530

    changed library app

--- node@13nov2019/LibrayApp/app.js
+++ node@13nov2019/LibrayApp/app.js
@@ -1,14 +1,15 @@
 const express=require('express');
 const chalk=require('chalk');
-const path=require("path")
-
+const path=require("path");
+const bodyparser=require('body-parser');
+const cors=require('cors');
 ////create object 
 
 // app.all('/',function(req,res){
 //   ////send just sending a data use send() for sending file use sendFile()
 //   res.sendFile(__dirname+"/src/views/index.html");
 
-  
+
 // });
 /////linking css with out a view engine
 // app.use(express.static(path.join(__dirname,"/public")));
@@ -16,9 +17,8 @@ const path=require("path")
 //   res.sendFile(path.join(__dirname,"src/views/index.html"));
 // });
 
-
 ////to add ejs change file extention of html  to .ejs
-var app=new express();
+
 
 var nav=[
   {
@@ -28,6 +28,10 @@ var nav=[
   {
     link:"/books",
     tittle:"BOOKS"
+   },
+   {
+    link:"/books/addbook",
+    tittle:"ADD BOOK"
    },
   {
     link:"/authers",
@@ -46,54 +50,28 @@ var nav=[
     tittle:"LOGIN"
   }
  ];
- var books=[
-          {
-            tittle:"book1",
-            genere:"history",
-            author:"victor hugo",
-            img:"user-icon1.png"
-          }, 
-          {
-            tittle:"book1",
-            genere:"history",
-            author:"victor hugo",
-            img:"user-icon1.png"
-          }, 
-          {
-            tittle:"book1",
-            genere:"history",
-            author:"victor hugo",
-            img:"user-icon1.png"
-          }, 
-          {
-            tittle:"book1",
-            genere:"history",
-            author:"victor hugo",
-            img:"user-icon1.png"
-          }, 
-        ];
-const booksrouter=express.Router();
-app.use('/books',booksrouter);
+ 
+var app=new express();
 
+const bookrouter=require('./src/routes/bookroute')(nav);
+const autherroute=require('./src/routes/autherroute')(nav);
 
-app.use(express.static(path.join(__dirname,"/public")));
-app.set('views','./src/views');
-app.set('view engine','ejs');
-booksrouter.route('/').get(
-  (req,res)=>{
-     res.render(
-       'books',
-       {
-        nav,tittle:"Books",books
-       }
-     )
-  });
+app.use(bodyparser.json());
+app.use(cors())
+app.use(bodyparser.urlencoded({extented:true}));/////to get value of form data
+app.use('/authers',autherroute);
+app.use('/books',bookrouter);
+app.use('/addbook',bookrouter);
 app.get('/',function(res,res){
   res.render('index',
    {
      nav:nav,tittle:"Library App"
   });
 });
+
+app.use(express.static(path.join(__dirname,"/public")));
+app.set('views','./src/views');
+app.set('view engine','ejs');
 
 ////set port 
 app.listen(3000,function(){
---
+++ node@13nov2019/LibrayApp/src/routes/autherroute.js
@@ -0,0 +1,90 @@
+const express=require('express');
+
+var authers=[
+
+              {
+               name:"M. T. Vasudevan Nair",
+               disc:`Madath Thekkepaattu Vasudevan Nair, popularly known as MT, is an Indian
+                      author,screenplay writer and
+                      film director. He is a prolific and versatile 
+                      writer in modern Malayalam literature, and is one of the masters of
+                      post-Independence Indian literature.`,
+               awards:`Padma Bhushan, Jnanpith Award, Sahitya Akademi Award, Kerala Sahitya Akademi Award`,
+               dob:"Born: 9 August 1933 (age 86 years), Kudallur",
+               books:"Naalukettu,Asuravithu,Manju,Kaalam and Randamoozham",
+               img:"VASUDEVAN_NAIR.jpg"
+              },
+              {
+                name:"Kamala Suraiyya",
+                disc:`Kamala Surayya, popularly known by her one-time pen name Madhavikutty
+                     and married name Kamala Das, was an Indian English poet as well as a
+                    leading Malayalam author from Kerala, India`,
+                awards:`Muttathu Varkey Award, Kerala Sahitya Akademi Award for Story, Kerala State Film Award for 
+                        Best Story, Vayalar Award`,
+                dob:"31 March 1934, Punnayurkulam",
+                books:`1964: Pakshiyude Manam (short stories)
+                1966: Naricheerukal Parakkumbol (short stories)
+                1968: Thanuppu (short story)
+                1982: Ente Katha (autobiography)
+                1987: Balyakala Smaranakal (childhood memoirs)
+                1989: Varshangalkku Mumbu (novel)
+                1990: Palayan (novel)
+                1991: Neypayasam (short story)
+                1992: Dayarikkurippukal (novel)
+                1994: Neermathalam Pootha Kalam (novel)
+                1996: Kadal Mayooram (short novel)
+                1996: Rohini (short novel)
+                1996: Rathriyude Padavinyasam (short novel)
+                1996: Aattukattil (short novel)
+                1996: Chekkerunna Pakshikal (short stories)`,
+                img:"Kamala_das.jpg"
+               },
+
+               {
+                name:"Vaikom Muhammad Basheer",
+                disc:`Vaikom Muhammad Basheer (21 January 1908 – 5 July 1995), 
+                      fondly known as Beypore Sultan, was an Indian independence
+                    activist and writer of Malayalam literature . He was a writer,
+                    humanist, freedom fighter, novelist and short story writer, 
+                    noted for his path-breaking, down-to-earth style of writing
+                    that made him equally popular among literary critics as well
+                    as the common man. His notable works include Balyakalasakhi,
+                    Shabdangal, Pathummayude Aadu, Mathilukal, Ntuppuppakkoranendarnnu,
+                    anmadinam and Anargha Nimisham and the translations of his works into other languages have earned him worldwide acclaim. The Government of India awarded him the fourth highest civilian honour of the Padma Shri in 1982. He was also a recipient of the Sahitya Akademi Fellowship, Kerala Sahitya Akademi Fellowship, and the [Kerala State Film Award for Best Story]`,
+                awards:`1970 Sahitya Akademi Fellowship
+                        1970 Kerala Sahitya Akademi Fellowship
+                        1970 Kerala Sahitya Akademi Award
+                        1982 Padma Shri
+                        1989 Kerala State Film Award for Best Story
+                        1992 Lalithambika Antharjanam Award
+                        1993 Muttathu Varkey Award
+                        1993 Vallathol Award`,
+                dob:"21 January 1908, Thalayolaparambu",
+                books:`Premalekhanam,Balyakalasakhi,Ntuppuppakkoranendarnnu,Pathummayude Aadu`,
+                img:"vykyam-muhammad-basheer.jpg"
+               },
+          ];
+
+var autherrouter=express.Router();
+ function router (nav) {
+         autherrouter.route('/').
+            get((req,res)=>{
+             res.render('authers',
+             {nav,tittle:"Authors",authers}
+            )
+            });
+            autherrouter.route('/:id')
+              .get((req,res)=>{
+                const id=req.params.id;
+                 res.render('auther',
+                      {
+                        nav,
+                        tittle:"Auther",
+                        auther:authers[id]
+                      }
+                 );
+              });
+         return  autherrouter;
+   }        
+   
+   module.exports=router;
